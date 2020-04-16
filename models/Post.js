21321d75b61e620205fe881658f264c7066e233a
Taufik <taufik.algi99@gmail.com>
2020-04-16 23:10:49 +0700

    feat: add models

---
+++ models/Post.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const postSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    body: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    }
+  },
+  { timesStamps: true }
+);
+
+const Post = mongoose.model('Post', postSchema);
+module.exports = Post;
