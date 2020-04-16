aa301b334a18c4ec7412bd41f193c193c7eae406
Taufik <taufik.algi99@gmail.com>
2020-04-16 23:11:04 +0700

    feat: add controllers

---
+++ controllers/PostsController.js
@@ -0,0 +1,41 @@
+const Post = require('../models/Post');
+
+const createPost = async (req, res, next) => {
+  const { title, body, author } = req.body;
+  const newPost = new Post({ title, body, autho});
+  try {
+    await newPost.save();
+    res.send({
+      message: 'Success! A new post has been created',
+      post: newPost
+    });
+  } catch (err) {
+    const error = new Error('Post could not be created');
+    error.error = err;
+    next(error);
+  }
+}
+
+const getAllPosts = async (req, res, next) => {
+  try {
+    const posts = await Post.find({});
+    res.send({ message: 'Success! All posts have been queried', posts});
+  } catch (err) {
+    const error = new Error('An error occured');
+    error.error = err;
+    next(error);
+  }
+}
+
+const getSinglePosts = async (req, res, next) => {
+  try {
+    const post = await Post.findOne({ _id: req.body.id });
+    res.send({ message: 'Success! A post has been queried', post});
+  } catch (err) {
+    const error = new Error('An error occured');
+    error.error = err;
+    next(error);
+  }
+};
+
+module.exports = { getAllPosts, getSinglePosts, createPost };
