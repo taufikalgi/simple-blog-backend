b3fe6f26fac312ccf2a8dcd91ee5d19036911730
Taufik <taufik.algi99@gmail.com>
2020-04-16 23:07:33 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+
+const app = express();
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+
+// DB Connection
+mongoose
+  .connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then( () => console.log('DB Connected'))
+  .catch((error) => console.log(error));
+
+// Middlewares
+app.use(express.json);
+
+// Routes
+const postsRoutes = require('./routes/posts');
+app.use('/api/posts', postsRoutes);
+
+// 404
+app.user((req, res, next) => {
+  const error = new Error('URL Not Found');
+  error.status = 404;
+  next(error);
+})
+
+// Error Handling
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.send({ message: `Error! ${error.message}`, error: error});
+})
+
+const port = process.env.PORT || 8000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
